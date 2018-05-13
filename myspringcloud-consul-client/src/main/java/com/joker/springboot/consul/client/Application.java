69d5d680f938c71797b4af28baab99a1c5635fdd
JokerG <545367987@qq.com>
2018-05-13 23:04:11 +0800

    eureka consul consumer 提交

--- myspringcloud-consul-client/src/main/java/com/joker/springboot/consul/client/Application.java
+++ myspringcloud-consul-client/src/main/java/com/joker/springboot/consul/client/Application.java
@@ -1,9 +1,12 @@
 package com.joker.springboot.consul.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * consul 启动命令：consul agent -dev
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
